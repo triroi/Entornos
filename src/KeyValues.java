83dae4e29f490c3a5c5c0c98482324255ac24002
InesTrigo <ines.trigor@iesfm.onmicrosoft.com>
2023-03-15 12:31:28 +0100

    15/03

--- src/KeyValues.java
+++ src/KeyValues.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -10,19 +11,33 @@ public class KeyValues {
         for (int i=0;i<tablero.size();i++){
             System.out.println(tablero.get((i)));
         }
+        //for en map
         /*for(Map.Entry<Integer,String>entry:tablero.entrySet()){
             Integer key= entry.getKey();
             String value = entry.getValue();
             System.out.println(key+" = "+value);
         }
-
          */
-        Iterator<Map.Entry<Integer,String>>iterator=tablero.entrySet().iterator();
-        while (iterator.hasNext()){
-            Map.Entry<Integer,String> entry=iterator.next();
-            Integer key = entry.getKey();
-            String value= entry.getValue();
-            System.out.println(key + " = "+value);
+        //iterator en map
+      /*Iterator<Map.Entry<Integer,String>>iterator=tablero.entrySet().iterator();
+      while (iterator.hasNext()){
+          Map.Entry<Integer,String> entry=iterator.next();
+          Integer key = entry.getKey();
+          String value= entry.getValue();
+          System.out.println(key + " = "+value);
+      }
+
+       */
+
+        //da error
+        ArrayList<String>array=new ArrayList<>();
+        array.add(0,"Griezzman");
+        array.add(4,"Menphis");
+
+        for(int i=0;i<20;i++){
+            System.out.println(array.get(i));
         }
+
+
     }
 }
